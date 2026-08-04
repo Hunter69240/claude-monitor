c88b0ffbde944f2f493f5bc01d1d60448a65e0ce
Hunter69240 <aadishds@gmail.com>
2026-08-04 22:55:09 +0530

    Explore email MIME structure and extract plain text body

--- app/email/fetch.py
+++ app/email/fetch.py
@@ -5,7 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def fetch_emails():
-
     email=os.getenv('GMAIL_MAIL')
     pwd=os.getenv('GMAIL_PASSWORD')
 
@@ -27,3 +26,54 @@ def fetch_emails():
     mail.logout()
 
     return email_ids
+
+email_ids=[b'31099',
+b'31100',
+b'31101',
+b'31118',
+b'31119',
+b'31120']
+
+print(email_ids)
+
+import imaplib
+import email
+from email.policy import default
+
+# Configuration
+imap_server = "imap.gmail.com"
+username = os.getenv('GMAIL_MAIL')
+password = os.getenv('GMAIL_PASSWORD')
+target_message_id = "noreply@statuspage.io" # The specific Message-ID header value
+
+# Connect
+mail = imaplib.IMAP4_SSL(imap_server)
+mail.login(username, password)
+mail.select("inbox")
+
+e_id=b'31099'
+try:
+        # Fetch by ID
+        status, msg_data = mail.fetch(e_id, '(RFC822)')
+        print("MSG_DATA")
+        
+        if status == 'OK' and msg_data[0][1]:
+            raw_email = msg_data[0][1]
+            msg = email.message_from_bytes(raw_email , policy=default)
+            print(dir(msg))
+            print(f"ID: {e_id.decode()} | Subject: {msg['Subject']}")
+            print(msg.is_multipart())
+
+            for i in msg.walk():
+                if i.get_content_type() == "text/plain":
+                    body = i.get_content()
+                    print(body)
+            
+        else:
+            print(f"ID: {e_id.decode()} | Not found or error.")
+            
+except Exception as e:
+        print(f"Error fetching ID: {e}")
+
+mail.close()
+mail.logout()   
